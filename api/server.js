ad28d1e2f1e54262804088ea693f829a424ef951
zaineb125 <zainebkefi520@gmail.com>
2023-05-31 12:13:30 +0100

    fix high vulnerabilities

--- api/server.js
+++ api/server.js
@@ -10,8 +10,22 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const fs = require("fs");
+const RateLimit = require("express-rate-limit");
+const validator = require("validator");
+
+function sanitizeInput(input) {
+  // Use validator's escape function to sanitize input
+  const sanitizedInput = validator.escape(input);
+
+  return sanitizedInput;
+}
 const app = express();
 require("dotenv").config();
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5,
+});
 
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
@@ -20,6 +34,8 @@ app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
+// apply rate limiter to all requests
+app.use(limiter);
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -128,6 +144,7 @@ app.put("/post", upload.single("file"), async (req, res) => {
   let newPath = null;
   if (req.file) {
     const { originalname, path } = req.file;
+    const pathSanitized = sanitizeInput(path);
     const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
     newPath = path + "." + ext;
@@ -138,12 +155,16 @@ app.put("/post", upload.single("file"), async (req, res) => {
   jwt.verify(token, secret, {}, async (e, info) => {
     if (e) throw e;
     const { id, title, summary, content } = req.body;
+    const idSanitized = sanitizeInput(id);
+    const titleSanitized = sanitizeInput(title);
+    const summarySanitized = sanitizeInput(summary);
+    const contentSanitized = sanitizeInput(content);
     const postDoc = await Post.findByIdAndUpdate(
-      id,
+      idSanitized,
       {
-        title,
-        summary,
-        content,
+        title: titleSanitized,
+        summary: summarySanitized,
+        content: contentSanitized,
         cover: newPath ? newPath : postDoc.cover,
       },
       { new: true }
